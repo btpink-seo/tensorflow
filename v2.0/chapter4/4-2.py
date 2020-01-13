99a53a4a2ddc091f61849d83993981f1550379ef
btpink <btpink.seo@gmail.com>
2020-01-13 21:50:15 +0900

    4-2

--- v2.0/chapter4/4-2.py
+++ v2.0/chapter4/4-2.py
@@ -23,33 +23,28 @@ b = tf.Variable(tf.zeros([3]), trainable=True, dtype=tf.float32)
 def hypothesis(x):
     return tf.nn.relu(tf.add(tf.matmul(x, W), b))
 
-# L = tf.add(tf.matmul(x_data, W), b)
-# L = tf.nn.relu(L)
-
-# 배열 내의 결괏값들을 전체 합이 1이 되도록 다듬어줌
-model = tf.nn.softmax(hypothesis(x_data))
+model = hypothesis(x_data)
 
 # 교차 엔트로피 함수
-def loss():
-    return tf.reduce_mean(-tf.reduce_sum(y_data * tf.math.log(model), axis=1))
+loss = lambda: tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_data, logits=hypothesis(x_data)))
 
 # 경사하강법
 optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
 
 # 학습
-for step in range(100):
+for step in range(1000):
     optimizer.minimize(loss, var_list=[W, b])
 
-#     if (step + 1) % 10 == 0:
-#         print(step + 1, loss().numpy())
+    if (step + 1) % 10 == 0:
+        print(step + 1, loss().numpy())
 
-# # 결과
-# prediction = tf.argmax(model, axis=1)
-# target = tf.argmax(y_data, axis=1)
-# print('예측값 : ', prediction.numpy())
-# print('실측값 : ', target.numpy())
+# 결과
+prediction = tf.argmax(tf.nn.softmax(model), axis=1)
+target = tf.argmax(y_data, axis=1)
+print('예측값 : ', prediction.numpy())
+print('실측값 : ', target.numpy())
 
-# # 정확도
-# is_correct = tf.equal(prediction, target)
-# accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-# print('정확도: %.2f' % accuracy.numpy() * 100)
+# 정확도
+is_correct = tf.equal(prediction, target)
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+print('정확도: %.2f' % accuracy.numpy())
